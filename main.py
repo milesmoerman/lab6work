e35a302ab50353f610eece8bc4e9d811fed07dc0
Alan <askrypek@ufl.edu>
2023-10-25 20:59:42 -0400

    code

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ def encoder(password):
         else:
             new_string += str(int(i) + 3)
     return new_string
+
+#Alan Skrypek
+def decoder(x):
+    old_pass = ""
+    for i in x:
+        if int(i) - 3 < 0:
+            old_pass += str((int(i) - 3) % 10)
+        else:
+            old_pass += str(int(i) -3)
+    return old_pass
 if __name__ == "__main__":
     print(encoder("00009962"))
     print(decoder("33332295"))
