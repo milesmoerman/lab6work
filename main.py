6194315299316bc0bd6598007d0d13b149adf45b
milesmoerman <milesmoerman@gmail.com>
2023-10-25 21:38:17 -0400

    test

--- main.py
+++ main.py
@@ -17,5 +17,21 @@ def decoder(x):
             old_pass += str(int(i) -3)
     return old_pass
 if __name__ == "__main__":
-    print(encoder("00009962"))
-    print(decoder("33332295"))
+    password = "123456"
+    while True:
+        print("Menu")
+        print("----------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+        opt = input("Please enter an option: ")
+        if int(opt) == 1:
+            password = input("Please enter your password to encode: ")
+            print("Your password has been encoded and stored!")
+            print()
+        elif int(opt) == 2:
+            print(f"The encoded password is {encoder(password)}, and the original password is {decoder(encoder(password))}.")
+            print()
+        else:
+            break
